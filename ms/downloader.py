4e11747318c5200b4ab3451884756e8072526119
pawelofficial <zdunekpawel@protonmail.com>
2025-01-12 21:33:32 +0100

    logs

--- ms/downloader.py
+++ ms/downloader.py
@@ -22,25 +22,32 @@ class Downloader:
     # downloads historical data 
     def download_historical_data(self
                                  ,ticker='SPX'
-                                 ,start_ts = '2024-01-01' # yyyy-mm-dd 
-                                 ,end_ts   = '2025-01-01' 
+                                 ,start_ts = '1990-01-01' # yyyy-mm-dd 
+                                 ,end_ts   = 'today' 
                                  ,interval = '1d'                
                                  ,save=True      
                                  ):
-        # if today - use today 
+        self.logger.info(f'Downloading data for {ticker} from {start_ts} to {end_ts} with interval {interval}')
+        
         if end_ts=='today':
-            end_ts= pd.to_datetime(pd.to_datetime(pd.Timestamp.now().normalize()))
-            
+            end_ts= pd.to_datetime(pd.to_datetime(pd.Timestamp.now().normalize()))    
+        
         start_ts = pd.to_datetime(start_ts) if start_ts is not None else None
         end_ts = pd.to_datetime(end_ts) if end_ts is not None else None
         
         if start_ts is None or end_ts is None:
             raise('ts cant be none ')
 
+        self.logger.info('trying to download ... ')
         data = yf.download(self.tickers_map[ticker], start=start_ts, end=end_ts, interval=interval)
+        self.logger.info(f'downloaded {data.shape} data ')
+        
+        self.logger.info('changing data structure')
         data.columns = data.columns.droplevel('Ticker')  # Drop the 'Ticker' level
         data.reset_index(inplace=True)
         data.columns=[col.lower() for col in data.columns]
+
+        
         if 'datetime' in data.columns:
             data['unixtime']=data['datetime'].astype('int64') / 10**9
             data['datetime']=pd.to_datetime(data['datetime'])
@@ -54,8 +61,14 @@ class Downloader:
             print('no date column in data ')
             print(data.columns)
             exit(1)
+
+        self.logger.info('changing structure ok ')
         if save:
+            self.logger.info('saving data')
             data.to_csv(os.path.join(self.data_path,f'{ticker}.csv'))
+        else:
+            self.logger.info('not saving data')
+
         return data 
 
         
